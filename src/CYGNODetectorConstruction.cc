26fd65bbba96dc9f6e075ba407e04262d3446971
Giulia D'Imperio <giulia.dimperio@roma1.infn.it>
2025-12-17 14:02:09 +0000

    fix geometry path

--- src/CYGNODetectorConstruction.cc
+++ src/CYGNODetectorConstruction.cc
@@ -49,7 +49,7 @@
 //#include "CYGNOBiasMultiParticleChangeCrossSection.hh"
 
 CYGNODetectorConstruction::CYGNODetectorConstruction() :
-   CYGNOGeomPath("../geometry_ASCII/lime_new"),
+   CYGNOGeomPath("../geometry/lime_new_ASCII/"),
    rockThicknessOuter(-999*m),
    rockThicknessInner(-999*m),
    //rockThicknessInner(4.*m),
